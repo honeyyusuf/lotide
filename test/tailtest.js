bd245037a671b04ec5fa9d3317226ef6e3040772
honeyyusuf <yusufhon0@gmail.com>
2020-11-01 08:28:20 +0000

    moch test for tail

--- test/tailtest.js
+++ test/tailtest.js
@@ -1,13 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("Returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("Returns [5] for []", () => {
+    assert.deepEqual(tail([5]), []);
+  });
+  
+  it("Returns Labs for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("Returns Labs for [Yo Yo, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse","Labs"]);
+  });
+
+  
+});
+
+
+

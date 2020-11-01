712d96e574d1f08fe4de11541b86feefbc02fb3d
honeyyusuf <yusufhon0@gmail.com>
2020-11-01 07:39:24 +0000

    checks if values are equal

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,9 @@
 const assertEqual = function(actual,expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertion failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !==  ${expected}`);
   }
 
 };
-
-// TEST CODE
-assertEqual('Lighthouse Labs','BootCamp');
-assertEqual(1,1);
+module.exports = assertEqual;

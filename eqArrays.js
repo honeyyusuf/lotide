4bedadd986ad1f0024c0cbbfff2324648130099a
honeyyusuf <yusufhon0@gmail.com>
2020-11-02 03:44:51 +0000

    check for equal arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,22 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !==  ${expected}`);
-  }
 
-};
 
 const eqArrays = function(arra1,arra2) {
   let x = true;
-  for (let i = 0; i < arra1.length; i++) {
-    if (arra1[i] !== arra2[i]) {
-      x = false;
+  if (arra1.length !== arra2.length) {
+    x = false;
+  } else {
+    for (let i = 0; i < arra1.length; i++) {
+      if (arra1[i] !== arra2[i]) {
+        x = false;
+      }
     }
   }
+  
   return x;
 };
-
     
-  
+module.exports = eqArrays;
    
   
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]) , false);
+

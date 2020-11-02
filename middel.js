84a8aeef017729d0a5f16952035f7d6c0a4a6100
honeyyusuf <yusufhon0@gmail.com>
2020-11-02 03:49:40 +0000

    find the middel of array

--- middel.js
+++ middel.js
@@ -1,26 +1,5 @@
-const eqArrays = function(arra1,arra2) {
-  let x = true;
-  if (arra1.length !== arra2.length) {
-    x = false;
-  } else {
-    for (let i = 0; i < arra1.length; i++) {
-      if (arra1[i] !== arra2[i]) {
-        x = false;
-      }
-    }
-  }
-  
-  return x;
-};
 
-const assertArraysEqual = function(actual , expected) {
-  let cheArr = eqArrays(actual, expected);
-  if (cheArr === true) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !==  ${expected}`);
-  }
-};
+
 const middle = function(array) {
   let mid = [];
   let arMi = Math.round(array.length / 2) - 1;
@@ -35,10 +14,5 @@ const middle = function(array) {
   return mid;
   
 };
+module.exports = middle;
 
-assertArraysEqual(middle([1]) ,[]);
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4]) , [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]) , [3, 4]);

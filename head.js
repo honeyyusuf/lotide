bf91185e9c12633a0ba3433a0ae7b947acd1a6be
honeyyusuf <yusufhon0@gmail.com>
2020-11-02 03:48:08 +0000

    finds the head of the file

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !==  ${expected}`);
-  }
 
-};
+const assertEqual = require('./assertEqual');
 const head = function(arr) {
   if (arr.lenght === 0) {
     return undefined;
@@ -14,5 +8,4 @@ const head = function(arr) {
   }
 
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
